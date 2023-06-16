e807f81c21488fb7327b93546e8355f456a706df
Ahmed Jamal <87934810+EngGemy99@users.noreply.github.com>
2023-06-16 21:29:38 +0300

    added wish list page

--- client/src/data/allData.jsx
+++ client/src/data/allData.jsx
@@ -566,3 +566,136 @@ export const locations = [
     rating: 4.5,
   },
 ];
+
+export const wishList = [
+  {
+    id: 1,
+    locationImages: [
+      {
+        id: 1,
+        url: "https://images.unsplash.com/photo-1600240644455-3edc55c375fe?auto=format&fit=crop&w=400&h=250&q=60",
+      },
+      {
+        id: 2,
+        url: "https://images.unsplash.com/photo-1653408400816-af6dba0c9432?auto=format&fit=crop&w=400&h=250&q=60",
+      },
+      {
+        id: 3,
+        url: "https://images.unsplash.com/photo-1653312727964-736f11663ef6?auto=format&fit=crop&w=400&h=250&q=80",
+      },
+      {
+        id: 4,
+        url: "https://images.unsplash.com/photo-1629447236132-22c57cd0f0bf?auto=format&fit=crop&w=400&h=250&q=60",
+      },
+    ],
+    location: "Gardon Reveira, Italy",
+    days: "Oct 2-9",
+    price: "$14,999 CAD night",
+    isNew: true,
+    rating: 4.5,
+  },
+  {
+    id: 2,
+    locationImages: [
+      {
+        id: 1,
+        url: "https://images.unsplash.com/photo-1469474968028-56623f02e42e?auto=format&fit=crop&w=400&h=250&q=60",
+      },
+      {
+        id: 2,
+        url: "https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?auto=format&fit=crop&w=400&h=250&q=60",
+      },
+      {
+        id: 3,
+        url: "https://images.unsplash.com/photo-1433086966358-54859d0ed716?auto=format&fit=crop&w=400&h=250&q=80",
+      },
+      {
+        id: 4,
+        url: "https://images.unsplash.com/photo-1501854140801-50d01698950b?auto=format&fit=crop&w=400&h=250&q=60",
+      },
+    ],
+    location: "Joshua tree, USA",
+    days: "Sep 2-11",
+    price: "$3000 CAD night",
+    isNew: false,
+    rating: 4.99,
+  },
+  {
+    id: 3,
+    locationImages: [
+      {
+        id: 1,
+        url: "https://images.unsplash.com/photo-1426604966848-d7adac402bff?auto=format&fit=crop&w=400&h=250&q=60",
+      },
+      {
+        id: 2,
+        url: "https://images.unsplash.com/photo-1586348943529-beaae6c28db9?auto=format&fit=crop&w=400&h=250&q=60",
+      },
+      {
+        id: 3,
+        url: "https://images.unsplash.com/photo-1465146344425-f00d5f5c8f07?auto=format&fit=crop&w=400&h=250&q=80",
+      },
+      {
+        id: 4,
+        url: "https://images.unsplash.com/photo-1472214103451-9374bd1c798e?auto=format&fit=crop&w=400&h=250&q=60",
+      },
+    ],
+    location: "Uvita rosa, Costa Rica",
+    days: "Nov 19-22",
+    price: "$1,129 CAD night",
+    isNew: true,
+    rating: 4.6,
+  },
+  {
+    id: 4,
+    locationImages: [
+      {
+        id: 1,
+        url: "https://images.unsplash.com/photo-1518173946687-a4c8892bbd9f?auto=format&fit=crop&w=400&h=250&q=60",
+      },
+      {
+        id: 2,
+        url: "https://images.unsplash.com/photo-1475113548554-5a36f1f523d6?auto=format&fit=crop&w=400&h=250&q=60",
+      },
+      {
+        id: 3,
+        url: "https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?auto=format&fit=crop&w=400&h=250&q=80",
+      },
+      {
+        id: 4,
+        url: "https://images.unsplash.com/photo-1505144808419-1957a94ca61e?auto=format&fit=crop&w=400&h=250&q=60",
+      },
+    ],
+    location: "Navidad, Chile",
+    days: "Sep 13-18",
+    price: "$208 CAD night",
+    isNew: false,
+    rating: 4.2,
+  },
+  {
+    id: 5,
+    locationImages: [
+      {
+        id: 1,
+        url: "https://images.unsplash.com/photo-1647891940243-77a6483a152e?auto=format&fit=crop&w=400&h=250&q=60",
+      },
+      {
+        id: 2,
+        url: "https://images.unsplash.com/photo-1604537529428-15bcbeecfe4d?auto=format&fit=crop&w=400&h=250&q=60",
+      },
+      {
+        id: 3,
+        url: "https://images.unsplash.com/photo-1587502537104-aac10f5fb6f7?auto=format&fit=crop&w=400&h=250&q=80",
+      },
+      {
+        id: 4,
+        url: "https://images.unsplash.com/photo-1587502537815-0c8b5c9ba39a?auto=format&fit=crop&w=400&h=250&q=60",
+      },
+    ],
+    location: "Paraty, Brazil",
+    days: "Aug 1-6",
+    price: "$243 CAD night",
+    isNew: true,
+    rating: 4.1,
+  },
+];

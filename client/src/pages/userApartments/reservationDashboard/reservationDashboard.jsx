c55b23f7e29c03f23eaea150c6a249252b2a9fca
Ahmed Ghames <93997953+AhmedMohamed652000@users.noreply.github.com>
2023-07-09 06:20:37 +0300

    edit ReservationDashboard

--- client/src/pages/userApartments/reservationDashboard/reservationDashboard.jsx
+++ client/src/pages/userApartments/reservationDashboard/reservationDashboard.jsx
@@ -1,25 +1,20 @@
-import Timeline from 'react-calendar-timeline'
-// make sure you include the timeline stylesheet or the timeline will not be styled
-import 'react-calendar-timeline/lib/Timeline.css'
-import moment from 'moment'
-import React from 'react'
-import { useState } from 'react'
+import React from 'react';
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useEffect } from 'react'
-import Roomster from '../../../API/config.jsx'
-
+import { useEffect } from 'react';
+import moment from 'moment';
+import Roomster from '../../../API/config.jsx';
+import { styled } from '@mui/material/styles';
 
 const ReservationDashboard = () => {
-
     const [apartments, setApartments] = useState([]);
     const [groups, setGroups] = useState([]);
     const [items, setItems] = useState([]);
 
     const { user } = useSelector((state) => state.user);
 
-
     const getUserApartments = async () => {
-        if (user._id !== "") {
+        if (user._id !== '') {
             try {
                 const { data } = await Roomster.get(`user/${user._id}/apartments`);
                 setApartments(data);
@@ -29,12 +24,9 @@ const ReservationDashboard = () => {
         }
     };
 
-
     useEffect(() => {
         getUserApartments();
-    }, []);
-
-
+    }, [user._id]);
 
     useEffect(() => {
         const groups = apartments?.map((apartment) => ({
@@ -49,24 +41,44 @@ const ReservationDashboard = () => {
                     group: apartment._id,
                     title: reservation.reservationId,
                     start_time: moment(reservation.startDate),
-                    end_time: moment(reservation.endDate)
-                }
-            })
-            return displayReservations
+                    end_time: moment(reservation.endDate),
+                };
+            });
+            return displayReservations;
         });
-        setItems(items)
-    }, [apartments])
+        setItems(items);
+    }, [apartments,user._id]);
 
     return (
         <div>
-            <Timeline
-                groups={groups}
-                items={items}
-                defaultTimeStart={moment().add(-12, 'hour')}
-                defaultTimeEnd={moment().add(12, 'hour')}
-            />
-        </div>
-    )
-}
+            <table className="table table-bordered border border-1 border-dark collaps" style={{background:'#ddf4cd'}} >
+                <thead className="thead-dark">
+                    <tr>
+                        <th className='h4 text-center ' style={{background:'#7dc240'}} colSpan={Math.floor(items.length / 2 -1)}>Apartments</th>
+                        <th className='h4 text-center ' style={{background:'#7dc240'}} colSpan={Math.floor(items.length / 2)}>Reservations</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {groups.map((group) => (
+                        <tr key={group.id} style={{marginBottom: '10px'}}>
+                            <td className='w-content table-warning border-1 border-dark w-25 text-center p-3 '>{group.title}</td>
+                            {items.map((item) =>
+                                item.group === group.id && (
+                                    <td className="table-success border border-1 border-dark text-center p-3"  key={`${group.id}-${item.id}`}>
+                                        <div className='centerItem h-100' >
+                                            {moment(item.start_time).format('MMM D')} -{' '}
+                                            {moment(item.end_time).format('MMM D, YYYY')}
+                                        </div>
+                                    </td>
+                                )
 
-export default ReservationDashboard
+                            )}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default ReservationDashboard;
